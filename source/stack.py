679cb0c5fbc4cd8acac59f4fd48a3c9913bc36f1
Justin Sitter <justin@jaytria.com>
2018-01-21 18:51:43 -0800

    passed a test

--- source/stack.py
+++ source/stack.py
@@ -22,26 +22,63 @@ class LinkedStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
+        if self.list.size == 0:
+            return True
+        else:
+            return False
+
 
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
+        return self.list.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Why? [append method appends to tail 
+        which is a constant time operation]"""
         # TODO: Push given item
+        self.list.append(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
         # TODO: Return top item, if any
+        if self.list.tail:
+            return self.list.tail.data
+        else:
+            return None
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return top item, if any
+        if self.list.size <= 0:
+            raise ValueError("Stack is empty")
+
+        item_index = self.list.size - 1
+
+        cur_node = self.list.head
+
+        if self.list.size > 1:
+            for _ in range(0, item_index):
+                cur_node = cur_node.next
+            
+            node_before = cur_node
+            self.list.tail = node_before
+            return_value = node_before.next.data
+            node_before.next = None
+            self.list.size -= 1
+        
+        else:
+            #only one item left in stack
+            return_value = cur_node.data
+            self.list.head = None
+            self.list.tail = None
+            self.list.size -= 1
+
+
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
