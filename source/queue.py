8ab9fc91875bef4924588d69c91ca645604a4919
Justin Sitter <justin@jaytria.com>
2018-01-21 21:12:59 -0800

    Pass all queue linked list tests

--- source/queue.py
+++ source/queue.py
@@ -22,26 +22,58 @@ class LinkedQueue(object):
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
         # TODO: Check if empty
+        if self.list.size == 0:
+            return True
+        else:
+            return False
 
     def length(self):
         """Return the number of items in this queue."""
         # TODO: Count number of items
+        return self.list.size
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Insert given item
+        self.list.prepend(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
+        if self.list.size == 0:
+            return None
+        else:
+            return self.list.get_at_index(self.list.size - 1)
 
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return front item, if any
+
+        if self.list.size == 0:
+            raise ValueError("Queue is empty")
+        
+        if self.list.size == 1:
+            return_value = self.list.head.data
+            self.list.delete(return_value)
+            return return_value
+
+        previous_node = self.list.head
+
+        for _ in range(0, self.list.size - 2):
+            previous_node = previous_node.next
+
+        return_value = previous_node.next.data
+        self.list.tail = previous_node
+        previous_node.next = None
+        self.list.size -= 1
+        return return_value
+
+        
+
 
 
 # Implement ArrayQueue below, then change the assignment at the bottom
