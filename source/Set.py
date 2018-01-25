bd4969a55203a4e5534c8d2c3180f38b11ebc777
Justin Sitter <justin@jaytria.com>
2018-01-24 17:50:58 -0800

    add methods

---
+++ source/Set.py
@@ -0,0 +1,24 @@
+#!python
+
+class Set():
+    def __init__(self, elements=None):
+    '''
+    Initialize a new empty set structure and add 
+    each element if a sequence is given.
+    '''
+        self.size = 0
+    
+    
+
+    def contains(element):
+    '''
+    return a boolean indicating whether element is
+    in this set
+    '''
+        pass
+    
+    def remove(element):
+        
+
+if __name__ == "__main__":
+    print("sets everywhere")
