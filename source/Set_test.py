ce6ffec6d20632141e1ee91fe4487b8ee64405ad
Justin Sitter <justin@jaytria.com>
2018-01-28 15:33:29 -0800

    added add to set test

--- source/Set_test.py
+++ source/Set_test.py
@@ -1,1 +1,17 @@
 import unittest
+from Set import Set
+
+class set_test(unittest.TestCase):
+
+    def test_set_add(self):
+        car_parts = Set()
+        car_parts.add("Smuffler")
+        assert(car_parts)
+        assert(len(car_parts.get_items())) is 1
+        car_parts.add("Noodler")
+        assert(len(car_parts.get_items())) is 2
+        car_parts.add("Smuffler")
+        assert(len(car_parts.get_items())) is 2
+        assert("Noodler" in car_parts.get_items())
+        assert("Smuffler" in car_parts.get_items())
+        
